5c9857c9c022ede1ed1b2948724656e9b8c42940
Bruna Nunes <bruna.f.n@hotmail.com>
2021-09-04 22:19:02 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,21 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box
-        mr="4"
-        textAlign="right"
-      >
-        <Text>Bruna Fraga</Text>
-        <Text color="gray.300" fontSize="small">bruna.f.n@hotmail.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box
+          mr="4"
+          textAlign="right"
+        >
+          <Text>Bruna Fraga</Text>
+          <Text color="gray.300" fontSize="small">bruna.f.n@hotmail.com</Text>
+        </Box>
+      )}
       <Avatar size="md" name="Bruna Fraga" src="https://github.com/brunafin.png" />
     </Flex>
   );
