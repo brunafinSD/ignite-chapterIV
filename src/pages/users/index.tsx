15410784ea74273e403791b9db736e0b6a2aea72
Bruna Nunes <bruna.f.n@hotmail.com>
2021-09-10 09:59:17 -0300

    listagem responsiva

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,14 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
   return (
     <Box>
       <Header />
@@ -33,17 +37,17 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
                 <Th></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -52,9 +56,9 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">bruna.f.n@hotmail.com</Text>
                   </Box>
                 </Td>
-                <Td>04 de Setembro, 2021</Td>
+                {isWideVersion && <Td>04 de Setembro, 2021</Td>}
                 <Td>
-                  <Button
+                {isWideVersion && <Button
                     as="a"
                     size="sm"
                     fontSize="sm"
@@ -62,11 +66,11 @@ export default function UserList() {
                     leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                   >
                     Editar
-                  </Button>
+                  </Button>}
                 </Td>
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -75,9 +79,9 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">bruna.f.n@hotmail.com</Text>
                   </Box>
                 </Td>
-                <Td>04 de Setembro, 2021</Td>
+                {isWideVersion && <Td>04 de Setembro, 2021</Td>}
                 <Td>
-                  <Button
+                {isWideVersion && <Button
                     as="a"
                     size="sm"
                     fontSize="sm"
@@ -85,11 +89,11 @@ export default function UserList() {
                     leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                   >
                     Editar
-                  </Button>
+                  </Button>}
                 </Td>
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -98,9 +102,10 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">bruna.f.n@hotmail.com</Text>
                   </Box>
                 </Td>
-                <Td>04 de Setembro, 2021</Td>
+                {isWideVersion &&
+                  <Td>04 de Setembro, 2021</Td>}
                 <Td>
-                  <Button
+                  {isWideVersion && <Button
                     as="a"
                     size="sm"
                     fontSize="sm"
@@ -108,7 +113,7 @@ export default function UserList() {
                     leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                   >
                     Editar
-                  </Button>
+                  </Button>}
                 </Td>
               </Tr>
             </Tbody>
